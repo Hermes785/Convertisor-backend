52d9437a0dcd211490d039581d1fd38fe39fc368
Hermes785 <hermesnguefack785@gmail.com>
2025-02-08 04:10:41 +0100

    updated producer,kafka

--- routes/convertDocsToPdfRoute.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express')
-const { ConvertDocxsToPdf } = require('../CovertDocsToPdf')
-const { sendRequestIdAfterUpload } = require('../requestIdController')
-const router = express.Router()
-
-router.post('/convertDocxsToPdf', sendRequestIdAfterUpload)
-
-module.exports = router
